466f1ba9311572118ae279dccf9d2b8a1caa7589
gordonk <gpkarlsson72@gmail.com>
2023-02-02 17:33:34 -0600

    working on main functionality

--- index.js
+++ index.js
@@ -85,8 +85,89 @@ function menuApp() {
           addIntern();
           break;
         default:
-          buildTeam()
+          makeTeam()
       }
     });
   }
+
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'engineerName',
+          message: "What is the engineer's name?"
+        },
+        {
+          type: 'input',
+          name: 'engineerId',
+          message: "What is the engineer's id?"
+        },
+        {
+          type: 'input',
+          name: 'engineerEmail',
+          message: "What is the engineer's email?"
+        },
+        {
+          type: 'input',
+          name: 'enginnerGithub',
+          message: "What is the engineer's Github username?"
+        }
+      ])
+      .then((answers) => {
+        const engineer= new Engineer(
+          answers.engineerName,
+          answers.engneerId,
+          answers.engineerEmail,
+          answers.engineerGithub
+        );
+      })
+      teamMembers.push(engineer);
+      idArray.push(answers.engineerId);
+      createTeam(); //createTeam? not makeTeam?
+  }
+  function addIntern() {
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'internName',
+        message: "What is the intern's name?"
+      },
+      {
+        type: 'input',
+        name: 'internId',
+        message: "What is the intern's id?"
+      },
+      {
+        type: 'input',
+        name: 'internEmail',
+        message: "What is the intern's email?"
+      },
+      {
+        type: 'input',
+        name: 'internSchool',
+        message: "What is the intern's school?"
+      }
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.internName,
+        answers.internId,
+        answers.internEmail,
+        answers.internSchool
+      );
+      teamMembers.push(intern);
+      idArray.push(internId);
+      createTeam();
+    });
+  }
+  function makeTeam() {
+    if (!fs.existsSync(directoryDist)) {
+      fs.mkdirSync(distPath, render(teamMembers))
+    }
+  }
+  createManager();
 }
+
+menuApp();
